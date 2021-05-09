a5008d44e3a1ae384e694826f38d815d388f29d8
Zdyrski <56028692+Zdyrski@users.noreply.github.com>
2021-05-09 20:30:54 +0200

    Add files via upload

---
+++ Zadanie2/Message2.java
@@ -0,0 +1,13 @@
+package Zadanie2;
+
+public class Message2{
+    private String message;
+
+    public Message2(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ Zadanie3/Message3.java
@@ -0,0 +1,13 @@
+package Zadanie3;
+
+public class Message3 {
+    private String message;
+
+    public Message3(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
